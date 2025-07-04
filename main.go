e58d4144ccb48c2b9071c08d8f6c7945f2cb7b3e
egorafedorov <egorafedorovme@gmail.com>
2025-07-04 12:13:56 +0300

    iteration 01

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func CreateBin(id string, private bool, name string) Bin {
+	return Bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+}
+
+func CreatedBinList() BinList {
+	return BinList{
+		Bins: make([]Bin, 0),
+	}
+}
 
 func main() {
 	fmt.Print("Start")
