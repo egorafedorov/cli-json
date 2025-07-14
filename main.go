5a4be4301d351c68d1007495a3ca384ea5d22118
thekalugin <thekalugin@gmail.com>
2025-07-14 19:18:46 +0300

    start project

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("Start")
+	color.Blue("Start")
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Console app for upload JSON files to the cloud and retrieve their content
