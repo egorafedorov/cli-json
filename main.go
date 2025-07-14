a6538657c72e5d069015ec24ddad7d90656cc858
thekalugin <thekalugin@gmail.com>
2025-07-14 19:24:25 +0300

    create bin and binlist

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/fatih/color"
 )
+
+type Bin struct {
+	Id        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func CreateBin(id string, private bool, name string) *Bin {
+	return &Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+}
+
+func CreateBinList() *BinList {
+	return &BinList{
+		Bins: make([]Bin, 0),
+	}
+}
 
 func main() {
 	fmt.Println("Start")
